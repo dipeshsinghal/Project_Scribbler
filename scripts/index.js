3d84a5951315d2a2594a000be3820f92a0faac21
Dipesh Singhal <dipesh.singhal@gmail.com>
2019-11-22 23:56:43 +0530

    Home Page added with Create Post modal

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+function signup_modal(show) {
+    if( show ) {
+        document.getElementById('id01').style.display='block';
+    } else {
+        document.getElementById('id01').style.display='none';
+    }
+}
+function signin_modal(show) {
+    if( show ) {
+        document.getElementById('id02').style.display='block';
+    } else {
+        document.getElementById('id02').style.display='none';
+    }
+}
+function post_modal(show) {
+    if( show ) {
+        document.getElementById('id03').style.display='block';
+    } else {
+        document.getElementById('id03').style.display='none';
+    }
+}
