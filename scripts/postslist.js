9d8e9dfac03e2c4565a3d12426c479685f90fdcd
Dipesh Singhal <dipesh.singhal@gmail.com>
2019-11-23 13:14:22 +0530

    Adding a Sample Post card and basic delete post confirm message

---
+++ scripts/postslist.js
@@ -0,0 +1,7 @@
+function deletepost(show) {
+    if( show ) {
+        document.getElementById('id04').style.display='block';
+    } else {
+        document.getElementById('id04').style.display='none';
+    }
+}
