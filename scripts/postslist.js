b1545b0e83d09e5cf00c8f091be83e4873b89359
Dipesh Singhal <dipesh.singhal@gmail.com>
2019-11-26 20:25:24 +0530

    Adding post card delete functionality

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,7 +1,16 @@
-function confirm_modal(show) {
+var node_id = null;
+
+function confirm_modal(show, delete_node_id) {
     if( show ) {
         document.getElementById('id04').style.display='block';
     } else {
         document.getElementById('id04').style.display='none';
     }   
+
+    node_id = delete_node_id;
 }
+
+function delete_post() {
+    document.getElementById(node_id).remove();
+}
+
